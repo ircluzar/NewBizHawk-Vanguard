a8e4b6867e1b5052243e57b44d8827071d11903b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-10 01:22:23 +0800

    Delete src directory

--- src/BizHawk.Emulation.Cores/Consoles/Nintendo/Gameboy/Gambatte.ILinkable.cs
+++
@@ -1,9 +0,0 @@
-﻿using BizHawk.Emulation.Common;
-
-namespace BizHawk.Emulation.Cores.Nintendo.Gameboy
-{
-	public partial class Gameboy : ILinkable
-	{
-		public bool LinkConnected { get; set; }
-	}
-}
